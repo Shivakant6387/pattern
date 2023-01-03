fb1032ff028b2b5cb0b00bea5d5a8794122ffaec
Shivakant6387 <shivakant5875@gmail.com>
2023-01-03 11:50:15 +0530

    Added all pattern file usning java pattern

---
+++ src/main/java/org/example/Alphabet_C.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Alphabet_C {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number of the n");
+        int n=scanner.nextInt();
+    }
+}
